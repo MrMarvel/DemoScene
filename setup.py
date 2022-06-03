5bd75b3e8daf0712043cc26b2d68d5bce947af1c
MrMarvel <seregakkk999@yandex.ru>
2022-06-03 20:07:25 +0300

    removed windows-cursor

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("LICENSE.txt", "r", encoding='utf-8') as fh:
     license_from_file = fh.read()
 setup(
     name='demoscene-mrmarvel',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(where='src'),  # ,
     package_dir={'': 'src'},
     url='',
@@ -12,5 +12,5 @@ setup(
     author='Sergey Kulikov',
     author_email='seregakkk999@yandex.ru',
     description='Demo-scene for class work',
-    install_requires=["cursor", "numpy", "windows-curses", "fps-limiter"]
+    install_requires=["cursor", "numpy", "fps-limiter"]
 )
